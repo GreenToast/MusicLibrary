4b7f591c81d69f59327fc8dfeed9c5f6206a3373
GreenToast <dennisneuendorf@gmail.com>
2015-04-25 00:01:10 +0200

    updated scripts

--- MusicLibFrontend/gulpfile.js
+++ MusicLibFrontend/gulpfile.js
@@ -1,104 +1,75 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var connect = require('gulp-connect');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
 var ngAnnotate = require('gulp-ng-annotate');
 var del = require('del');
+var recursive = require('recursive-readdir');
+var rmdir = require( 'rmdir' );
+var fs = require('fs');
 
 var paths = {
   scripts: ['app/js/**/*.js'],
-  images: 'app/img/**/*'
+  images: 'app/img/**/*',
+  libs: ['app/lib/**/*.min.js','!app/lib/jqueryui/ui/**/*.min.js','app/lib/**/*.map'],
+  html: ['app/templates/**/*.*','app/index.html'],
+  css: ['app/css/**/*.css']
 };
 
 // Not all tasks need to use streams
 // A gulpfile is just another node program and you can use all packages available on npm
 gulp.task('clean', function(cb) {
-  // You can use multiple globbing patterns as you would with `gulp.src`
-  del(['build'], cb);
+    var basedir= 'build';
+    var files = fs.readdirSync(basedir);
+    files.forEach(function(dir){
+        del(basedir+'/'+dir);
+    });
+    cb();
 });
 
-gulp.task('scripts', ['clean'], function() {
-  // Minify and copy all JavaScript (except vendor scripts)
-  // with sourcemaps all the way down
+gulp.task('scripts', function() {
   return gulp.src(paths.scripts)
   .pipe(concat('app.js'))
       .pipe(ngAnnotate())
       .pipe(uglify())
-      //.pipe(sourcemaps.write())
     .pipe(sourcemaps.write('../js'))
     .pipe(gulp.dest('build/js'));
 });
 
-/*gulp.task('scripts', ['clean'], function() {
-  // Minify and copy all JavaScript (except vendor scripts)
-  // with sourcemaps all the way down
-  return gulp.src(paths.scripts)
-    .pipe(sourcemaps.init())
-      .pipe(uglify())
-      .pipe(concat('all.min.js'))
-    .pipe(sourcemaps.write('../js'))
-    .pipe(gulp.dest('build/js'));
-});*/
-
 // Copy all static images
-gulp.task('images', ['clean'], function() {
-  return gulp.src(paths.images)
+gulp.task('images', function() {
+  return gulp.src(paths.images, { base: './app/' })
     // Pass in options to the task
     .pipe(imagemin({optimizationLevel: 5}))
-    .pipe(gulp.dest('build/img')); 
+    .pipe(gulp.dest('build')); 
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-  gulp.watch(paths.scripts, ['scripts']);
-  gulp.watch(paths.images, ['images']);
+  gulp.watch('app/**/*.*', ['clean','scripts', 'images', 'move','reload']);
 });
 
-var filesToMove = [
-        'app/templates/**/*.*',
-        'app/index.html',
-        'app/css/**/*.*',
-        
-        //'!app/css/ext/*.*'
-    ];
+gulp.task('reload', function() {
+  gulp.src('app/**/*.*').pipe(connect.reload());
+});
 
-var angularlibsToMove = [
-        'node_modules/angular/**/*.min.js',
-        'node_modules/angular/**/*.map',
-        'node_modules/angular-route/**/*.min.js',
-        'node_modules/angular-route/**/*.map',
-        'node_modules/angular-route/**/*.min.js',
-        'node_modules/angular-route/**/*.map'
-    ];
-
-var bootstraplibsToMove = [
-        'node_modules/bootstrap/dist/**/*.min.js',
-    ];
-
-var jQuerylibsToMove = [
-        'node_modules/jquery/dist/**/*.min.js',
-        'node_modules/jquery/dist/**/*.map'
-    ];
-
-var jQueryUilibsToMove = [
-        'app/lib/**/*.min.js',
-    ];
 
 gulp.task('move',['clean'], function(){
-  // the base option sets the relative root for the set of files,
-  // preserving the folder structure
-    gulp.src(filesToMove, { base: './app/' })
-        .pipe(gulp.dest('build'));
-    gulp.src(angularlibsToMove, { base: './node_modules/' })
-        .pipe(gulp.dest('build/lib'));
-    gulp.src(bootstraplibsToMove, { base: './node_modules/bootstrap/dist/js' })
-        .pipe(gulp.dest('build/lib/bootstrap'));    
-    gulp.src(jQuerylibsToMove, { base: './node_modules/jquery/dist/' })
-        .pipe(gulp.dest('build/lib/jquery'));
-    gulp.src(jQueryUilibsToMove, { base: './app/' })
+    gulp.src(paths.html.concat(paths.css).concat(paths.libs), { base: './app/' })
         .pipe(gulp.dest('build'));
 });
 
+gulp.task('startServer',function(){ 
+    connect.server({
+        root: ['build'],
+        livereload: true,
+        port: 9000
+    });
+});
+
+
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'scripts', 'images', 'move']);
+gulp.task('run', ['clean','watch', 'scripts', 'images', 'move', 'startServer']);
+gulp.task('build', ['clean','scripts', 'images', 'move']);
